7b14d8b7b3e93017dd48784fd0f1ba9e9461327e
Bhaskar Latwal <bhaskarlatwalcr7@gmail.com>
2024-08-17 23:47:00 +0530

    Update Header.jsx

--- Header.jsx
+++ Header.jsx
@@ -1,4 +1,16 @@
 function Header(){
+    return(
+        <header>
+            <h1>MY WEBSITE</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Contacts</li>
+                    <li>About</li>
+                </ul>
+            </nav>
+        </header>
+    );
 
 }
 export default Header
